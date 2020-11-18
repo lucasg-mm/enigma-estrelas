84b7b830917b95b256e8089c7545a7b4403b64ad
Lucas Miranda <lucasalternativo.lg@gmail.com>
2020-11-18 20:05:05 -0300

    Refaz o código com solver de programação inteira

--- tsp.py
+++ tsp.py
@@ -1,24 +1,16 @@
-# LUCAS G. M. MIRANDA - 10265892 - lucasgmm@usp.br
-# MARCELA TIEMI SHINZATO - 10276953 - marcelats@usp.br
-# SÉRGIO RICARDO G. B. FILHO - 10408386 - sergiobarbosa@usp.br
-# TIAGO LASCALA AUDE - 8936742 - tiago.aude@usp.br
-
-
-# Resolve uma instância do problema do caixeiro viajante,
-# usando o CP-SAT solver do Google OR-Tools.
-
+from __future__ import print_function
 from collections import deque
-from ortools.sat.python import cp_model
+from ortools.linear_solver import pywraplp
 from math import sqrt
 
 
-# Dada uma matriz mat_adj, a função abaixo retorna uma lista de sets, cada um contendo os índices
-# que representam os vértices que formam um subciclo. Se não houver subciclos, retorna False. A
-# matriz mat_adj deve ser uma matriz adjacências de um grafo não ponderado, ou seja, mat_adj[i][j] == True,
-# se existe uma aresta entre os vértices i e j.
-
-
 def acha_subciclos(mat_adj):
+    """Dada uma matriz mat_adj, a função abaixo retorna uma lista de sets, cada um contendo os índices
+    que representam os vértices que formam um subciclo. Se não houver subciclos, retorna False. A
+    matriz mat_adj deve ser uma matriz adjacências de um grafo não ponderado, ou seja, mat_adj[i][j] == 1,
+    se existe uma aresta entre os vértices i e j.
+    """
+
     # se marcados[i] == True, então o vértice 1 já foi atribuido a um comp. conexo
     marcados = [False for i in range(len(mat_adj))]
     comp_conexos = []  # lista de sets que contêm os índices que formam comp. conexo
@@ -46,42 +38,50 @@ def acha_subciclos(mat_adj):
                 comp_conexos.append(aux)  # adiciona mais um set para a lista
     return comp_conexos
 
-# Resolve o Problema do Caixeiro-Viajante
 
+def resolve_tsp(coords):
+    """
+    Resolve o Problema do Caixeiro-Viajante.
+    """
+    solver = pywraplp.Solver.CreateSolver('SCIP')  # define o solver
+    num_galaxias = len(coords)  # armazena o número de galáxias
 
-def resolve_tsp(distancias):
-    # -- declara o modelo CP-SAT
-    model = cp_model.CpModel()
+    # -> definindo as variáveis da função objetivo
+    y = []  # variáveis da função objetivo (em forma de matriz)
+    for i in range(num_galaxias):
+        y.append([])
+        for j in range(num_galaxias):
+            y[i].append(solver.IntVar(0, 1, f"y[{i}][{j}]"))
+    print(f"Número de variáveis = {solver.NumVariables()}")
 
-    # -- cria as variáveis binárias inteiras x[i][j]
-    num_galaxias = len(distancias)
-    x = [[model.NewBoolVar(f"x[{i},{j}]") for j in range(
-        num_galaxias)] for i in range(num_galaxias)]
-
-    # -- cria as restrições
+    # -> definindo as restrições
     # garante que apenas uma aresta saia de um vértice
     for i in range(num_galaxias):
-        model.Add(sum(x[i][j] for j in range(num_galaxias)) == 1)
+        solver.Add(sum(y[i][j] for j in range(num_galaxias)) == 1)
 
     # garante que apenas uma aresta chegue em um vértice
     for j in range(num_galaxias):
-        model.Add(sum(x[i][j] for i in range(num_galaxias)) == 1)
+        solver.Add(sum(y[i][j] for i in range(num_galaxias)) == 1)
 
-    # -- cria a função objetivo
-    # termos da função objetivo
-    obj_termos = []
+    print(f"Número de restrições = {solver.NumConstraints()}")
+
+    # -> definindo a função objetivo
+    distancias = []
+    parcelas_obj = []
     for i in range(num_galaxias):
+        distancias.append([])
         for j in range(num_galaxias):
-            obj_termos.append(int(distancias[i][j])*x[i][j])
-    model.Minimize(sum(obj_termos))
+            distancias[i].append(round(dist_euclid(coords[i], coords[j])))
+            parcelas_obj.append(y[i][j] * distancias[i][j])
+    solver.Minimize(sum(parcelas_obj))
 
-    # -- resolve
-    solver = cp_model.CpSolver()  # obtém o solver
-    solver.Solve(model)  # resolve
+    # -> resolve
+    solver.Solve()
 
-    # obtém a solução parcial na forma de matriz (sol_parcial[i][j] == True se x[i][j] == True)
-    sol_parcial = [[solver.BooleanValue(x[i][j]) for i in range(
+    # obtém a solução parcial na forma de matriz
+    sol_parcial = [[y[i][j].solution_value() for i in range(
         num_galaxias)] for j in range(num_galaxias)]
+
     # se a sol_parcial não tem nenhum subciclo, ela é a correta!
     subciclos = acha_subciclos(sol_parcial)
 
@@ -92,11 +92,11 @@ def resolve_tsp(distancias):
             aux_list = []
             for i in subciclo:
                 for j in subciclo:
-                    aux_list.append(x[i][j])
-            model.Add(sum(aux_list) <= len(subciclo) - 1)
+                    aux_list.append(y[i][j])
+            solver.Add(sum(aux_list) <= len(subciclo) - 1)
 
-        solver.Solve(model)  # resolve novamente
-        sol_parcial = [[solver.BooleanValue(x[i][j]) for i in range(  # obtém outra solução parcial
+        solver.Solve()  # resolve novamente
+        sol_parcial = [[y[i][j].solution_value() for i in range(  # obtém outra solução parcial
             num_galaxias)] for j in range(num_galaxias)]
         # se a sol_parcial não tem nenhum subciclo, ela é a correta!
         subciclos = acha_subciclos(sol_parcial)
@@ -105,32 +105,22 @@ def resolve_tsp(distancias):
     print("Solução encontrada:")
     for i in range(num_galaxias):
         for j in range(num_galaxias):
-            if solver.BooleanValue(x[i][j]):
+            if y[i][j].solution_value():
                 print(f"De {i} para {j} -> custo: {distancias[i][j]}")
-    print(f"Custo total: {solver.ObjectiveValue()}")
-
-# calcula a distância euclidiana 2d entre dois pontos representados por tuplas
+    print(f"Custo total: {solver.Objective().Value()}")       
 
 
 def dist_euclid(a, b):
+    """
+    Calcula a distância euclidiana 2d entre dois pontos representados por tuplas.
+    """
     return sqrt(((a[0] - b[0])**2) + ((a[1] - b[1])**2))
-
-# constrói a matriz de distâncias
-
-
-def constroi_matriz(coords):
-    num_vertices = len(coords)
-    distancias = []
-    for i in range(num_vertices):
-        distancias.append([])
-        for j in range(num_vertices):
-            distancias[i].append(round(dist_euclid(coords[i], coords[j])))
-    return distancias
-
-# pega input e retorna numa lista de tuplas
 
 
 def get_input():
+    """
+    Pega input e retorna numa lista de tuplas.
+    """
     num_vertices = int(input())
     coords = []  # lista que contém cada coordenada em uma tupla
 
@@ -147,8 +137,7 @@ def get_input():
 
 def main():
     coords = get_input()
-    distancias = constroi_matriz(coords)
-    resolve_tsp(distancias)
+    resolve_tsp(coords)
 
 
 if __name__ == '__main__':
