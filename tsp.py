9f210440d78f015b1c94d5e232fb429f8e7bb314
Lucas Miranda <lucasalternativo.lg@gmail.com>
2020-11-28 18:28:28 -0300

    Cria variaçao levando em conta simetria

--- tsp.py
+++ tsp.py
@@ -2,12 +2,12 @@ from __future__ import print_function
 from collections import deque
 from ortools.linear_solver import pywraplp
 from math import sqrt
+from time import time
 # import igraph
 
 
 def acha_subciclos(mat_adj):
-    """
-    Dada uma matriz mat_adj, a função abaixo retorna uma lista de sets, cada um contendo os índices
+    """Dada uma matriz mat_adj, a função abaixo retorna uma lista de sets, cada um contendo os índices
     que representam os vértices que formam um subciclo. Se não houver subciclos, retorna False. A
     matriz mat_adj deve ser uma matriz adjacências de um grafo não ponderado, ou seja, mat_adj[i][j] == 1,
     se existe uma aresta entre os vértices i e j.
@@ -36,8 +36,10 @@ def acha_subciclos(mat_adj):
                         fila.append(j)
                         # o vértice j faz parte do mesmo componente conexo de marcados[i]
                         aux.add(j)
-            if 0 not in aux:
-                comp_conexos.append(aux)  # adiciona mais um set para a lista
+            # if 0 not in aux:
+            #     comp_conexos.append(aux)  # adiciona mais um set para a lista
+            if len(aux) >= 3 and len(aux) <= len(mat_adj) - 1:
+                comp_conexos.append(aux)  # adiciona mais um set para a lista                
     return comp_conexos
 
 
@@ -47,7 +49,7 @@ def resolve_tsp(coords):
     """
 
     # para plotar o grafo final:
-    # g = igraph.Graph(directed=True)
+    # g = igraph.Graph()
     # g.add_vertices(len(coords))
 
     solver = pywraplp.Solver.CreateSolver('SCIP')  # define o solver
@@ -58,17 +60,18 @@ def resolve_tsp(coords):
     for i in range(num_galaxias):
         y.append([])
         for j in range(num_galaxias):
-            y[i].append(solver.IntVar(0, 1, f"y[{i}][{j}]"))
+            if j >= i + 1:
+                y[i].append(solver.IntVar(0, 1, f"y[{i}][{j}]"))
+            else:
+                y[i].append(0)    
     print(f"Número de variáveis = {solver.NumVariables()}")
 
     # -> definindo as restrições
     # garante que apenas uma aresta saia de um vértice
-    for i in range(num_galaxias):
-        solver.Add(sum(y[i][j] for j in range(num_galaxias)) == 1)
-
-    # garante que apenas uma aresta chegue em um vértice
-    for j in range(num_galaxias):
-        solver.Add(sum(y[i][j] for i in range(num_galaxias)) == 1)
+    for vertice in range(num_galaxias):
+        arestas = [y[vertice][j] for j in range(vertice + 1, num_galaxias)]
+        arestas.extend((y[i][vertice] for i in range(0, vertice)))
+        solver.Add(sum(arestas) == 2)
 
     print(f"Número de restrições (inicialmente) = {solver.NumConstraints()}")
 
@@ -78,33 +81,56 @@ def resolve_tsp(coords):
     for i in range(num_galaxias):
         distancias.append([])
         for j in range(num_galaxias):
-            distancias[i].append(int(round(dist_euclid(coords[i], coords[j]))))
-            parcelas_obj.append(y[i][j] * distancias[i][j])
+            if j >= i + 1:
+                distancias[i].append(int(round(dist_euclid(coords[i], coords[j]))))
+                parcelas_obj.append(y[i][j] * distancias[i][j])
+            else:
+                distancias[i].append(0)    
     solver.Minimize(sum(parcelas_obj))
 
     # -> resolve
+    timeout = time() + 60*10  # seta timeout de 10 minutos (em segundos)
+    solver.set_time_limit(max(int((timeout - time())*1000), 0))
     solver.Solve()
+    solver.set_time_limit(max(int((timeout - time())*1000), 0))
 
     # obtém a solução parcial na forma de matriz
-    sol_parcial = [[y[i][j].solution_value() for i in range(
-        num_galaxias)] for j in range(num_galaxias)]
+    sol_parcial = []
+    for i in range(num_galaxias):
+        sol_parcial.append([])
+        for j in range(num_galaxias):
+            if j >= i + 1:
+                sol_parcial[i].append(y[i][j].solution_value())
+            elif j == i:
+                sol_parcial[i].append(0.0)
+            else:
+                sol_parcial[i].append(y[j][i].solution_value())  
 
     # se a sol_parcial não tem nenhum subciclo, ela é a correta!
     subciclos = acha_subciclos(sol_parcial)
-
-    while subciclos:
+    while subciclos and time() < timeout:
         # se a solução parcial possui subciclos, adiciona restrições para detectar
         # os subciclos achados, e resolve o problema novamente, achando outra solução parcial
         for subciclo in subciclos:
             aux_list = []
             for i in subciclo:
                 for j in subciclo:
-                    aux_list.append(y[i][j])
+                    if j >= i + 1:
+                        aux_list.append(y[i][j])
             solver.Add(sum(aux_list) <= len(subciclo) - 1)
 
+        solver.set_time_limit(max(int((timeout - time())*1000), 0))
         solver.Solve()  # resolve novamente
-        sol_parcial = [[y[i][j].solution_value() for i in range(  # obtém outra solução parcial
-            num_galaxias)] for j in range(num_galaxias)]
+        sol_parcial = []
+        for i in range(num_galaxias):
+            sol_parcial.append([])
+            for j in range(num_galaxias):
+                if j >= i + 1:
+                    sol_parcial[i].append(y[i][j].solution_value())
+                elif j == i:
+                    sol_parcial[i].append(0.0)
+                else:
+                    sol_parcial[i].append(y[j][i].solution_value())        
         # se a sol_parcial não tem nenhum subciclo, ela é a correta!
         subciclos = acha_subciclos(sol_parcial)
 
@@ -112,9 +138,9 @@ def resolve_tsp(coords):
     print("-->SOLUÇÃO FINAL")
     print("Solução encontrada:")
     for i in range(num_galaxias):
-        for j in range(num_galaxias):
+        for j in range(i + 1, num_galaxias):
             if y[i][j].solution_value():
-                print(f"De {i} para {j} -> custo: {distancias[i][j]}")
+                print(f"{i} -- {j} -> custo: {distancias[i][j]}")
                 custo_total += distancias[i][j]
                 # g.add_edge(i, j)
     print(f"Custo total: {round(solver.Objective().Value())}")
